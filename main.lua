4248fc7ca710d9af9ee9b1d6f62c8ce1bba5ad0d
Nathan McKnight <asaneoil@gmail.com>
2022-06-07 20:11:19 -0400

    Loaded background
    -Loaded the background. It is 2 lines because someone will have to use it and if I just load it, it won't be useable.
    
    -Added background asset.

--- main.lua
+++ main.lua
@@ -1,19 +1,14 @@
+local background
 
 function love.load(args)
-
-
-
+    background = love.graphics.newImage('assets/main_menu_background.png')
 end
 
 
 function love.update(dt)
-	
-
-
 end
 
 
 function love.draw()
     love.graphics.print('Welcome to the world of Pokémon!', 275, 275)
-
 end
